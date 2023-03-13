1d02893cc4f0708161c9d665777487b6234dba57
Songjeongmoon <wjdans5692@gmail.com>
2023-03-13 06:46:42 +0000

    FEAT: Add Page division

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,25 @@ $(".icon").mouseover((event) => {
 $(".icon").mouseleave((event) => {
   event.target.style.transform = "scale(1, 1)";
 });
+$(".categoryList").mouseover((event) => {
+  event.target.style.transform = "scale(1.1, 1.1)";
+})
+$(".categoryList").mouseleave((event) => {
+  event.target.style.transform = "scale(1, 1)";
+})
+$("#aboutMe").click(() => {
+  let page = $("#aboutPage").offset();
+  $("html").animate({scrollTop: page.top}, 400);
+})
+$("#skill").click(() => {
+  let page = $("#skillPage").offset();
+  $("html").animate({scrollTop: page.top}, 400);
+})
+$("#education").click(() => {
+  let page = $("#educationPage").offset();
+  $("html").animate({scrollTop: page.top}, 400);
+})
+$("#do").click(() => {
+  let page = $("#doPage").offset();
+  $("html").animate({scrollTop: page.top}, 400);
+})
